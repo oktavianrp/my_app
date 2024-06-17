fd63eb1e34ff303397c894d7508d018b53260824
oktavianrp <vianjr235@gmail.com>
2024-06-17 15:06:37 +0000

    selesai menambahkan comment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,19 @@ class HomeController < ApplicationController
   def index
     @articles = Article.all
   end
+  
+  def read 
+    @article = Article.where(id: params[:article_id]).first
+  end
+
+  def save_comment
+    @article = Article.where(id: params[:article_id]).first
+    comment = @article.comments.new
+    comment.comment = params[:comment]
+    if comment.save
+      redirect_to read_path(article_id: @article.id, notice: 'Success')
+    else 
+      redirect_to read_path(article_id: @article.id, notice: 'Error')
+    end
+  end
 end
